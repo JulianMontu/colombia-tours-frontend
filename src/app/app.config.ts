b3274f57564423e22dabceab19875c0c08a6126e
JulianMontu <julamontufar@misena.edu.co>
2025-03-23 22:27:46 -0500

    inicio del proyecto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { MyPreset } from './mypreset';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: false || 'none'
+        }
+      },
+      csp: {
+        nonce: '...'
+      }
+    })
+  ]
 };
