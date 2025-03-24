22eead93ce35781b075c7bdc4b4e9fa4a50b6ace
JulianMontu <julamontufar@misena.edu.co>
2025-03-24 18:07:00 -0500

    se agrega footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { PrimengModule } from '../../primeng/primeng.module';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule,RouterModule, PrimengModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
--- src/app/layouts/index/index.component.ts
+++ src/app/layouts/index/index.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-index',
-  imports: [CommonModule,HeaderComponent,RouterModule],
+  imports: [CommonModule,HeaderComponent,RouterModule, FooterComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
